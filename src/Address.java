788ac61a24b55175f6940f205de7b499835f83a1
HP <mdzaid1299@gmail.com>
2022-09-11 01:07:13 +0530

    initial

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Address {
+public class Address implements Comparable<Address> {
+    private String name;
+    private String street;
+    private String city;
+    private String country;
+    private  int zipCode;
+
+    private Address() {}
+
+    Address(String name, String street, String city, String country, int zipCode) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+        this.zipCode = zipCode;
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "name='" + name + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", zipCode=" + zipCode +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Address address) {
+        if (this.zipCode == address.zipCode) {
+            return 0;
+        }
+        if (this.zipCode < address.zipCode) {
+            return -1;
+        }
+        return 1;
+    }
 }
--- src/impl.java
+++ src/impl.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class impl {
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Scratch {
+    public static void main(String[] args) {
+        List<Address> reg = Arrays.asList(new Address ("Jane" , "marblle" , "gk", "joj",110065),
+        new Address ("Sara", "tl" , "hu" , "fd", 110054));
+        Collections.sort(reg);
+        for (Address no : reg) {
+            System.out.println(no);
+        }
+    }
 }
